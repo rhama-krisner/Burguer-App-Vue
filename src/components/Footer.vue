571f84cca070d0d5ce42023c2dd6765079d3c336
Rhama Krisner <rhamakrisner@gmail.com>
2023-08-10 22:17:28 -0300

    Adicionado estilo no navbar e no footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="footer">
+        <p>Make Your Burger &copy; 2023</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+    .footer{
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        display: flex;
+        color: #FCBA03;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
+</style>
